710c3f48c5f034a2291562833fa878634b655a46
DaiCae <925268575@qq.com>
2022-01-26 03:27:07 +0800

    FIX 启动时自动创建表

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 def init_app(app):
     db.init_app(app) # 数据库初始化
-
+    db.create_all(app = app)
 from .student import Student
 from .log import Log
 
--- models/log.py
+++ models/log.py
@@ -3,11 +3,13 @@ from . import db
 # 日志
 class Log(db.Model):
     __tablename__ = 'note_log'
-    id  = db.Column(db.Integer, primary_key = True)
-    uid = db.Column(db.Integer)
-    url = db.Column(db.String(200))
+    id   = db.Column(db.Integer, primary_key = True)
+    uid  = db.Column(db.Integer)
+    url  = db.Column(db.String(200))
+    time = db.Column(db.DateTime)
 
     def __init__(self, data):
         self.id  = data['id']
         self.uid = data['uid']
         self.url = data['url']
+        self.time = data['time']
